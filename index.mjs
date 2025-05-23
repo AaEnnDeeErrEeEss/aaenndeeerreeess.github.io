af42b0d0649eab46de5ef9ff1feaa81d6ef1c712
Andres Sulg <aaendeereees@gmail.com>
2025-05-23 18:32:16 +0300

    equipment status buttons, and input

--- index.mjs
+++ index.mjs
@@ -30,23 +30,10 @@ window.onload = () => {
     Object.entries(varustus).forEach(([ruum, elemendid]) => {
         const elRuum = createDetailsWithyTitle(ruum);
         if (Object.keys(elemendid).length === 0) {
-            elRuum.style.color = 'darkred';
+            elRuum.style.color = 'darkgrey';
         } else {
             Object.entries(elemendid).forEach(([element, tegevused]) => {
-                const elElement = createDetailsWithyTitle(element);
-                tegevused.forEach(tegevus => {
-                    const elTegevus = document.createElement('div');
-                    const tegevuseTekst = document.createElement('span');
-                    tegevuseTekst.innerText = tegevus.tegevus;
-                    if (tegevus.abitekst) {
-                        tegevuseTekst.innerText += `🖹`;
-                    }
-                    if (tegevus.abivideo) {
-                        tegevuseTekst.innerText += `📹`;
-                    }
-                    elTegevus.appendChild(tegevuseTekst);
-                    elElement.appendChild(elTegevus);
-                });
+                const elElement = createTegevusDetails(element, tegevused);
                 elRuum.appendChild(elElement);
             });
         }
@@ -61,4 +48,66 @@ function createDetailsWithyTitle(title) {
     summary.innerText = title;
     details.appendChild(summary);
     return details;
+}
+
+function createTegevusDetails(element, tegevused) {
+    const elElement = document.createElement('details');
+    const summary = document.createElement('summary');
+    // not ok input
+    const veateade = document.createElement('input');
+    veateade.type = 'text';
+    veateade.placeholder = 'Sisesta puuduse kirjeldus';
+    veateade.style.display = 'none';
+    // not ok
+    const notOk = document.createElement('span');
+    notOk.innerText = '❌';
+    notOk.addEventListener('click', () => {
+        summary.style.color = 'darkred';
+        veateade.style.display = 'block';
+        veateade.focus();
+    });
+    // ok
+    const ok = document.createElement('span');
+    ok.innerText = '✅';
+    ok.addEventListener('click', () => {
+        summary.style.color = 'darkgreen';
+        veateade.style.display = 'none';
+    });
+    // ?
+    const question = document.createElement('span');
+    question.innerText = '🟡';
+    question.addEventListener('click', () => {
+        summary.style.color = 'darkorange';
+        veateade.style.display = 'none';
+    });
+
+    summary.innerText = element;
+    elElement.appendChild(ok);
+    elElement.appendChild(question);
+    elElement.appendChild(notOk);
+    elElement.appendChild(veateade);
+    elElement.appendChild(summary);
+
+    tegevused.forEach(tegevus => {
+        const elAlamtegevus = document.createElement('div');
+        // Abitekst
+        if (tegevus.abitekst) {
+            const tegevuseTekst = document.createElement('span');
+            tegevuseTekst.innerText = `🖹`;
+            elAlamtegevus.appendChild(tegevuseTekst);
+        }
+        // Abivideo
+        if (tegevus.abivideo) {
+            const tegevuseVideo = document.createElement('span');
+            tegevuseVideo.innerText = `📹`;
+            elAlamtegevus.appendChild(tegevuseVideo);
+        }
+        // Tekst
+        const tegevuseTekst = document.createElement('span');
+        tegevuseTekst.innerText = ` ${tegevus.tegevus}`;
+        elAlamtegevus.appendChild(tegevuseTekst);
+        elElement.appendChild(elAlamtegevus);
+    });
+
+    return elElement;
 }
